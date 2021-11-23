00c3a8d87ac216a19440776977dc787fb37366ea
mVladislavs <92714105+mVladislavs@users.noreply.github.com>
2021-11-23 17:30:31 +0200

    Update GenSet.py

--- GenSet.py
+++ GenSet.py
@@ -3,6 +3,7 @@ def GenSet(frequency:int, amplitude:float, channel:bool):
     import dwf
     import sys
 
+    # Check input values
     if frequency > 5e7:
         sys.exit("Frequency cannot be > 50MHz")
     
@@ -19,7 +20,8 @@ def GenSet(frequency:int, amplitude:float, channel:bool):
         sys.exit("Choose the right channel:\nFalse - 1st channel\nTrue - 2nd channel")
 
     #open device as analog outpput
-    AO = dwf.DwfAnalogOut()  
+    AO = dwf.DwfAnalogOut()
+    
     #confugure the channel
     AO.nodeEnableSet(channel, AO.NODE.CARRIER, True)
     AO.nodeFunctionSet(channel, AO.NODE.CARRIER, AO.FUNC.SINE)
