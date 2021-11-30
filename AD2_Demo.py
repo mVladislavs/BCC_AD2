521972c99edc31d8221b9993949748e5a1c2ac38
mVladislavs <92714105+mVladislavs@users.noreply.github.com>
2021-11-30 15:08:30 +0200

    Update AD2_Demo.py

--- AD2_Demo.py
+++ AD2_Demo.py
@@ -1,37 +1,28 @@
 from AD2_functions import *
 from threading import *
 
-gen1 = Thread(target=GenSet, args=(3e6, 1, False),)
-gen2 = Thread(target=GenSet, args=(3e6, 2, False),)
-gen3 = Thread(target=GenSet, args=(3e6, 3, False),)
+Device = AD2init()
+
+gen1 = Thread(target=GenSet, args=(3e6, 1, False, Device[0]),)
+gen2 = Thread(target=GenSet, args=(3e6, 2, False, Device[0]),)
+gen3 = Thread(target=GenSet, args=(3e6, 5, False, Device[0]),)
 
 gen1.start()
-time.sleep(1)
 for i in range(5):
-    P2P = GetP2P(2000, False)
+    P2P = GetP2P(8192, False, Device[1])
     print(P2P)
 gen1.join()
 
 gen2.start()
-time.sleep(1)
 for i in range(5):
-    P2P = GetP2P(2000, False)
+    P2P = GetP2P(8192, False, Device[1])
     print(P2P)
 gen2.join()
 
 gen3.start()
-time.sleep(1)
 for i in range(5):
-    P2P = GetP2P(2000, False)
+    P2P = GetP2P(8192, False, Device[1])
     print(P2P)
 gen3.join()
 
-
-
-# GenSet(3e6,1,False)
-# GetP2P(2000,False)
-
-
-
-
-
+AD2close(Device[0], Device[1])
